72e66c5b23faa82acee3aaeaf135aedda770978a
JaeoCho <cho5095@kookmin.ac.kr>
2018-05-28 13:17:15 +0900

    environ

--- swp1_01.py
+++ swp1_01.py
@@ -1,8 +1,19 @@
 from wsgiref.simple_server import make_server
 
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type','text/plain')])
-    yield 'Hello World\n'
+    	response_body = [
+        	'%s: %s' % (key,value) for key, value in sorted(environ.items())
+    	]
+    	response_body = '\n'.join(response_body)
+	
+	status = '200 OK'
+	response_headers = [
+		('Content-Type','text/plain'),
+		('Content-Length', str(len(response_body)))
+	]
+
+	start_response(status, response_headers)
+	return [response_body]
 
 
 httpd = make_server('localhost',8051,application)
