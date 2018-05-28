e176fe055020aba6d42532b153ee5389544f9321
JaeoCho <cho5095@kookmin.ac.kr>
2018-05-28 13:19:10 +0900

    path

--- swp1_01.py
+++ swp1_01.py
@@ -2,12 +2,16 @@ from wsgiref.simple_server import make_server
 from cgi import parse_qs
 
 def application(environ, start_response):
+	path = environ['PATH_INFO'].split('/')
 	request_body_size = int(environ.get('CONTENT_LENGTH','0'))
 	request_body = environ['wsgi.input'].read(request_body_size)
 	d = parse_qs(request_body)
 #	d = parse_qs(environ['QUERY_STRING'])
 	name = d.get('name',[''])[0]
 	age = d.get('age',[''])[0]
+
+	print('method : %s' % environ['REQUEST_METHOD'])
+	print('path : %s' %repr(path))
 	response_body = 'name : %s\nage : %s\n' %(name,age)
 	
 	status = '200 OK'
